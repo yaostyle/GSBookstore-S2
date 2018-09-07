280ae598079b779a5f3b38c64fadfcbcc725f492
yaostyle <yaostyle@gmail.com>
2018-09-06 20:14:52 -0700

    BookCursor complete.

--- app/src/main/java/com/android/chrishsu/gsbookstore/BookCursorAdapter.java
+++ app/src/main/java/com/android/chrishsu/gsbookstore/BookCursorAdapter.java
@@ -9,13 +9,15 @@ import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
+import com.android.chrishsu.gsbookstore.data.BookContract;
+
 public class BookCursorAdapter extends CursorAdapter{
     public BookCursorAdapter(Context context, Cursor c) {
         super(context, c, 0);
     }
 
     @Override
-    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
         return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
@@ -24,17 +26,17 @@ public class BookCursorAdapter extends CursorAdapter{
         TextView nameTextView = view.findViewById(R.id.name);
         TextView supplierTextView =  view.findViewById(R.id.supplier);
 
-        int nameColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_NAME);
-        int breedColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED);
+        int nameColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_PRODUCT_NAME);
+        int supplierColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_SUPPLIER_NAME);
 
-        String petName = cursor.getString(nameColumnIndex);
-        String petBreed = cursor.getString(breedColumnIndex);
+        String bookName = cursor.getString(nameColumnIndex);
+        String bookSupplier = cursor.getString(supplierColumnIndex);
 
-        if (TextUtils.isEmpty(petBreed)) {
-            petBreed = context.getString(R.string.unknown_breed);
+        if (TextUtils.isEmpty(bookName)) {
+            bookSupplier = context.getString(R.string.unknow_supplier);
         }
 
-        nameTextView.setText(petName);
-        summaryTextView.setText(petBreed);
+        nameTextView.setText(bookName);
+        supplierTextView.setText(bookSupplier);
     }
 }
