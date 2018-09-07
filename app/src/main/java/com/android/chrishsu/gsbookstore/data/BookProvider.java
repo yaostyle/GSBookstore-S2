c360294dd96313b2f6e9dbd057eecb8c04a051cc
yaostyle <yaostyle@gmail.com>
2018-09-06 21:58:15 -0700

    Setting up ContentProvider complete.

--- app/src/main/java/com/android/chrishsu/gsbookstore/data/BookProvider.java
+++ app/src/main/java/com/android/chrishsu/gsbookstore/data/BookProvider.java
@@ -1,6 +1,224 @@
 package com.android.chrishsu.gsbookstore.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.util.Log;
 
 public class BookProvider extends ContentProvider{
+
+    public static final String LOG_TAG = BookProvider.class.getSimpleName();
+    private static final int BOOKS = 100;
+    private static final int BOOK_ID = 101;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(BookContract.CONTENT_AUTHROITY
+                , BookContract.PATH_BOOKS, BOOKS);
+        sUriMatcher.addURI(BookContract.CONTENT_AUTHROITY
+                , BookContract.PATH_BOOKS + "/#", BOOK_ID);
+    }
+
+    // Database helper object
+    private BookDbHelper mDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        mDbHelper = new BookDbHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri,
+                        String[] projection,
+                        String selection,
+                        String[] selectionArgs,
+                        String sortOrder) {
+
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        Cursor cursor;
+
+        int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case BOOKS:
+                cursor = database.query(BookContract.BookEntry.TABLE_NAME
+                        ,projection
+                        ,selection
+                        ,selectionArgs
+                        ,null
+                        ,null
+                        ,sortOrder);
+                break;
+            case BOOK_ID:
+                selection = BookContract.BookEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                cursor = database.query(BookContract.BookEntry.TABLE_NAME
+                        , projection
+                        ,selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknow URI " + uri);
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case BOOKS:
+                return insertPet(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    private Uri insertPet(Uri uri, ContentValues values) {
+
+        String name = values.getAsString(BookContract.BookEntry.COLUMN_PRODUCT_NAME);
+        Double price = values.getAsDouble(BookContract.BookEntry.COLUMN_PRICE);
+        Integer qty = values.getAsInteger(BookContract.BookEntry.COLUMN_QTY);
+        String supplier = values.getAsString(BookContract.BookEntry.COLUMN_SUPPLIER_NAME);
+        String supplier_phone = values.getAsString(BookContract.BookEntry.COLUMN_SUPPLIER_PHONE);
+
+        if (name == null) {
+            throw new IllegalArgumentException("This book requires a name");
+        }
+
+        if (price <= 0) {
+            throw new IllegalArgumentException("This book requires a valid price.");
+        }
+
+        if (qty == null || qty <= 0) {
+            qty = 0;
+        }
+
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+        long id = database.insert(BookContract.BookEntry.TABLE_NAME, null, values);
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return ContentUris.withAppendedId(uri, id);
+    }
+
+    @Override
+    public int update(Uri uri,
+                      ContentValues contentValues,
+                      String selection,
+                      String[] selectionArgs) {
+
+        final int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case BOOKS:
+                return updatePet(uri, contentValues, selection, selectionArgs);
+            case BOOK_ID:
+                selection = BookContract.BookEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                return updatePet(uri, contentValues, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Update is not supported for " + uri);
+        }
+    }
+
+    private int updatePet(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        if (values.containsKey(BookContract.BookEntry.COLUMN_PRODUCT_NAME)) {
+            String name = values.getAsString(BookContract.BookEntry.COLUMN_PRODUCT_NAME);
+            if (name == null) {
+                throw new IllegalArgumentException("This book requires a name");
+            }
+        }
+
+        if (values.containsKey(BookContract.BookEntry.COLUMN_PRICE)) {
+            Double price = values.getAsDouble(BookContract.BookEntry.COLUMN_PRICE);
+            if (price <= 0) {
+                throw new IllegalArgumentException("This book requires valid price.");
+            }
+        }
+
+        if (values.containsKey(BookContract.BookEntry.COLUMN_QTY)) {
+            Integer qty = values.getAsInteger(BookContract.BookEntry.COLUMN_QTY);
+            if (qty == null || qty <= 0) {
+                throw new IllegalArgumentException("This book requires a qty.");
+            }
+        }
+
+        if(values.size() == 0) {
+            return 0;
+        }
+
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        int rowsUpdated = database.update(BookContract.BookEntry.TABLE_NAME, values, selection, selectionArgs);
+
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return rowsUpdated;
+    }
+
+    @Override
+    public int delete(Uri uri,
+                      String selection,
+                      String[] selectionArgs) {
+
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        final int match = sUriMatcher.match(uri);
+        int rowsDeleted;
+
+        switch (match) {
+            case BOOKS:
+                rowsDeleted = database.delete(BookContract.BookEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            case BOOK_ID:
+                selection = BookContract.BookEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                rowsDeleted = database.delete(BookContract.BookEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
+
+        if (rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return rowsDeleted;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case BOOKS:
+                return BookContract.BookEntry.CONTENT_LIST_TYPE;
+            case BOOK_ID:
+                return BookContract.BookEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalStateException("Unknown URI " + uri);
+        }
+    }
 }
